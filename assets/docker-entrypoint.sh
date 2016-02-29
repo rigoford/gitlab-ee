f92a89b4aa7fdc4f25d6e35fd43e681ea8d5fc51
Martin Ford <uncletravellingmart@hotmail.com>
2016-02-29 22:10:07 +0000

    Update docker-entrypoint.sh

--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# Configure keys only ssh connection
+sed -i.bak 's/#[ ]*RSAAuthentication yes/RSAAuthentication yes/' /etc/ssh/ssh_config
+sed -i.bak 's/#[ ]*PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/ssh_config
+rm /etc/ssh/ssh_config.bak
+/etc/init.d/ssh restart
+
+# Start Runit
 /opt/gitlab/embedded/bin/runsvdir-start &
 
+# (Re)configure GitLab
 gitlab-ctl reconfigure
 
+# Tail all the GitLab logs
 gitlab-ctl tail
